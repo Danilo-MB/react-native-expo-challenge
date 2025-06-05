e3bcac132e288c205e2b44fecdf83b62143f942a
Danilo Marino Bata <danilo@Danilos-MacBook-Pro.local>
2025-06-05 17:12:29 -0300

    Adds Linking to open user website in the user card.

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -1,12 +1,15 @@
 import { User } from '../schemas';
 import React from 'react';
 import { Card, Email, Name, Phone, Website } from '@/styled/userCard';
+import { Alert, Linking, TouchableOpacity } from 'react-native';
 
 type Props = {
   user: User;
 };
 
 const UserCard: React.FC<Props> = React.memo(({ user }: Props) => {
+  const url = user.website.startsWith('http') ? user.website : `https://${user.website}`;
+  
   return (
     <Card>
       <Name>
@@ -14,7 +17,15 @@ const UserCard: React.FC<Props> = React.memo(({ user }: Props) => {
       </Name>
       <Email>{user.email}</Email>
       <Phone>{user.phone}</Phone>
-      <Website>{user.website}</Website>
+      <TouchableOpacity
+        onPress={() => {
+          Linking.openURL(url).catch(() => {
+            Alert.alert('Error', 'Unable to open the website.');
+          });
+        }}
+      >
+        <Website>{user.website}</Website>
+      </TouchableOpacity>
     </Card>
   );
 });
